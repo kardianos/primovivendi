bf278b0fd2170c8384915e29bcd0bd286ef18dc8
Daniel Theophanes <kardianos@gmail.com>
2025-09-21 10:44:48 -0500

    book1-1: start book v1.1

---
+++ book1-1/chapters/00_title.md
@@ -0,0 +1,9 @@
+---
+title: "Procedural Objectivism"
+subtitle: Common Principles Toward a Reality-Based Life
+author: Daniel Theophanes
+date: 2025-07-04
+geometry: margin=2cm
+toc: true
+toc-depth: 2
+---
---
+++ book1-1/concat.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	const dir = "chapters"
+	const outputFile = "book.md"
+
+	// Read directory contents
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return fmt.Errorf("Error reading directory: %v", err)
+	}
+
+	var files []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		name := entry.Name()
+		if strings.HasSuffix(name, "_exp.md") {
+			continue
+		}
+
+		files = append(files, name)
+	}
+	sort.Strings(files)
+
+	var out = &bytes.Buffer{}
+	for i, file := range files {
+		if i > 0 {
+			_, err = out.WriteString("\n\n\\newpage\n\n")
+			if err != nil {
+				return fmt.Errorf("Error writing newline: %v", err)
+			}
+		}
+		fn := filepath.Join(dir, file)
+		in, err := os.Open(fn)
+		if err != nil {
+			return fmt.Errorf("Error opening file %s: %v", file, err)
+		}
+
+		_, err = io.Copy(out, in)
+		in.Close()
+		if err != nil {
+			return fmt.Errorf("Error reading file %s: %v", file, err)
+		}
+	}
+
+	return os.WriteFile(outputFile, out.Bytes(), 0600)
+}
